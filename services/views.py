159524b009609eb25a0052cc393ceadebb501b1d
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2025-02-25 21:19:19 +0000

    .

--- services/views.py
+++ services/views.py
@@ -2,25 +2,25 @@
 from django.shortcuts import render
 
 def service_list(request):
-    return render(request, 'services/list.html')
+    return render(request, 'pages/services/list.html')
 
 def digital_transformation(request):
-    return render(request, 'services/digital_transformation.html')
+    return render(request, 'pages/services/digital_transformation.html')  # Updated path
 
 def consulting(request):
-    return render(request, 'services/consulting.html')
+    return render(request, 'pages/services/consulting.html')
 
 def development(request):
-    return render(request, 'services/development.html')
+    return render(request, 'pages/services/development.html')
 
 def automation(request):
-    return render(request, 'services/automation.html')
+    return render(request, 'pages/services/automation.html')
 
 def research(request):
-    return render(request, 'services/research.html')
+    return render(request, 'pages/services/research.html')
 
 def career(request):
-    return render(request, 'services/career.html')
+    return render(request, 'pages/services/career.html')
 
 def global_talent(request):
-    return render(request, 'services/global_talent.html')
+    return render(request, 'pages/services/global_talent.html')
